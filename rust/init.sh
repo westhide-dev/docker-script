ed5ad9048802bf98aa5fbfba77aa823a34b8ac7b
westhide.dev <westhide.dev@gmail.com>
2024-09-27 22:53:44 +0800

    chore: use DK_USER

--- rust/init.sh
+++ rust/init.sh
@@ -5,8 +5,9 @@ set -e
 CUR_DIR=$(realpath $(dirname ${BASH_SOURCE:-0}))
 
 sudo apt-get update
-sudo apt-get install -y locales build-essential curl vim
-
+sudo apt-get install -y locales
 sudo localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
+
+sudo apt-get install -y build-essential ca-certificates openssh-client curl vim git
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain nightly -y
