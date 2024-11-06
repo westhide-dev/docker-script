92aadeec718e7dab7022a3e855c0be6c36289d0a
westhide.dev <westhide.dev@gmail.com>
2024-11-06 19:41:39 +0800

    WIP: rust

--- rust/entrypoint.sh
+++ rust/entrypoint.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -ex
 
-CUR_DIR=$(realpath $(dirname ${BASH_SOURCE:-0}))
+CUR_DIR=$(realpath "$(dirname "${BASH_SOURCE:-$0}")")
 
 bash
--- rust/init.sh
+++ rust/init.sh
@@ -1,13 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 set -e
 
-CUR_DIR=$(realpath $(dirname ${BASH_SOURCE:-0}))
+CUR_DIR=$(realpath "$(dirname "${BASH_SOURCE:-$0}")")
 
 sudo apt-get update
 sudo apt-get install -y locales
 sudo localedef -i en_US -c -f UTF-8 -A /usr/share/locale/locale.alias en_US.UTF-8
 
-sudo apt-get install -y build-essential ca-certificates openssh-client curl vim git
+sudo apt-get install -y ca-certificates openssh-client curl vim git git-lfs
+sudo apt-get install -y build-essential cmake libssl-dev pkg-config
+sudo apt-get install -y protobuf-compiler
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain nightly -y
